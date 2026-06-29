d99cd39d0fbd393df9acf56f5335930471ca2062
Dennis Weaver <dennis_weaver@sprich.com>
2026-06-29 09:44:01 -0400

    update to use postgreSQL

--- django_learning/settings.py
+++ django_learning/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "django_learning.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_learning",
+        "USER": "dweaver",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     },
 }
 
