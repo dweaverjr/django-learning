464e448492044b9cddf942d8f38898c907a36e78
Dennis Weaver <dennis_weaver@sprich.com>
2026-07-02 15:01:36 -0400

    switch back to sqllite

--- django_learning/settings.py
+++ django_learning/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = "django_learning.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django_learning",
-        "USER": "dweaver",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
 
