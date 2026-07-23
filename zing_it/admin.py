1ef8af4d662393821d9407aec60d9335b2814776
Dennis Weaver <dennis_weaver@sprich.com>
2026-07-23 08:46:07 -0400

    Add Company, Employee, Project models and register in admin
    - Add Company, Employee, Project models with FK/M2M relations
    - Register new models with admin site, set custom admin site header
    - Track migrations 0001-0004

--- zing_it/admin.py
+++ zing_it/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from zing_it.models import AccessRecord, Company, Employee, Project, Topic, Webpage
+
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
+admin.site.register(Company)  # Register the Company model with the admin site
+admin.site.register(Employee)  # Register the Employee model with the admin site
+admin.site.register(Project)  # Register the Project model with the admin site
+admin.site.site_header = "Zing It Admin"
